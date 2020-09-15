759c7f4d144daa8074f0f783751f185b4359960e
Carlos Franco <cafranco0505@gmail.com>
2020-09-14 22:31:31 -0500

    hostbinding in component

---
+++ src/app/lista-destinos/lista-destinos.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { DestinoViaje } from '../models/destino-viajes.models';
+
+@Component({
+  selector: 'app-lista-destinos',
+  templateUrl: './lista-destinos.component.html',
+  styleUrls: ['./lista-destinos.component.css']
+})
+export class ListaDestinosComponent implements OnInit {
+  destinos: DestinoViaje[];
+  constructor() {
+    this.destinos = [];
+  }
+
+  ngOnInit(): void {
+  }
+
+  guardar(n: string, u: string): boolean {
+    this.destinos.push(new DestinoViaje(n, u));
+    return false;
+  }
+
+}
