8f37c5b869923911359f184b49bc3f991f1140ee
Carlos Franco <cafranco0505@gmail.com>
2020-09-21 16:31:23 -0500

    observable

---
+++ src/app/models/destino-api-client.model.ts
@@ -0,0 +1,30 @@
+import { BehaviorSubject, Subject } from 'rxjs';
+import { DestinoViaje } from './destino-viajes.models';
+
+export class DestinosApiClient {
+  destinos: DestinoViaje[];
+  current: Subject<DestinoViaje> = new BehaviorSubject<DestinoViaje>(null);
+  constructor() {
+    this.destinos = [];
+  }
+  add(d:DestinoViaje){
+    this.destinos.push(d);
+  }
+  getAll(): DestinoViaje[] {
+    return this.destinos;
+  }
+  getById(id: String): DestinoViaje {
+    return this.destinos.filter(function(d){ return d.id.toString() === id; })[0];
+  }
+
+  elegir(d: DestinoViaje){
+    this.destinos.forEach(x => x.setSelected(false));
+    d.setSelected(true);
+    this.current.next(d);
+    }
+
+
+    subscribeOnChange(fn) {
+        this.current.subscribe(fn);
+    }
+}
